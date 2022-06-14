a5e2f309627e014161212be7b3e8c34474303888
vadimcimbalyk <vadymonua@gmail.com>
2022-06-14 16:59:09 +0300

    create project and connect npm modules

---
+++ gulpfile.js
@@ -0,0 +1,75 @@
+const {src, dest, watch, series, parallel} = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const pug = require('gulp-pug');
+const imagemin = require('gulp-imagemin');
+const connect = require("gulp-connect");
+const jsmin = require("gulp-jsmin");
+
+const appPath = {
+    scss: './app/scss/**/*.scss',
+    pug: './app/index.pug',
+    img:[
+        './app/images/**/*.jpg',
+        './app/images/**/*.png',
+        './app/images/**/*.svg',
+    ],
+    js:'./app/js/*.js'
+};
+const destPath = {
+    css: './dest/css',
+    html: './dest',
+    images:'./dest/images',
+    js:'./dest/js/'
+};
+
+const jsPath=[
+    './node_modules/jquery/dist/jquery.min.js',
+    './node_modules/bootstrap/dist/js/bootstrap.min.js',
+    './app/js/index.js'
+]
+
+function buildHtml() {
+    return src(appPath.pug)
+        .pipe(pug())
+        .pipe(dest(destPath.html))
+        .pipe(connect.reload())
+};
+
+function buildStyles() {
+    return src(appPath.scss)
+        .pipe(sass({outputStyle: "compressed"}).on('error', sass.logError))
+        .pipe(dest(destPath.css))
+        .pipe(connect.reload())
+};
+function imageMin() {
+    return src(appPath.img)
+        .pipe(imagemin())
+        .pipe(dest(destPath.img))
+        .pipe(connect.reload())
+};
+function jsMin(){
+    return src(jsPath)
+        .pipe(jsmin())
+        .pipe(dest(destPath.js))
+        .pipe(connect.reload());
+}
+
+function server(){
+    connect.server({
+        name:'Dev App',
+        root:'dest',
+        port:8080,
+        livereload: true
+    })
+}
+
+
+
+function watchCode() {
+    watch(appPath.scss, buildStyles);
+    watch(appPath.pug, buildHtml);
+    watch(appPath.img, imageMin);
+    watch(appPath.js, jsMin);
+}
+
+exports.default = series(buildStyles, buildHtml,jsMin, parallel(server,watchCode))
