93fb49d238344fb8e0fb30b11e4a831bbbf4abb9
vadimcimbalyk <vadymonua@gmail.com>
2022-06-20 15:53:29 +0300

    header navigation

--- app/js/index.js
+++ app/js/index.js
@@ -1,1 +1,8 @@
-alert("hello world")
+"use strict"
+const navbar = document.querySelector(".navbar-menu")
+document.querySelector('.toggle__menu').addEventListener("click",  (e)=>{
+    e.preventDefault();
+    e.target.classList.toggle("is-active");
+    navbar.classList.toggle("open")
+    document.querySelector(".section__header").classList.toggle("move__down");
+});
